22f8268dbd2fa9e4dc1dcba26561ebe1e70da50b
Aleksandr Chervonniy <misteralex@Aleksandrs-iMac.local>
2021-02-26 17:11:46 +0200

    added a responsive for header and header content

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function () {
 
 
+    $('.nav__gamburger').on('click', function (event) {
+        $('.nav__gamburger').toggleClass('nav__gamburger-active');
+        $('.nav__items').toggleClass('nav__items-active');
+        $('body').toggleClass('lock');
+    });
+
+
 
 });
+
