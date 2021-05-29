e689755d91e470bd793868f0c3fc1003dc973ffb
Aleksandr Chervonniy <misteralex@Aleksandrs-iMac.local>
2021-05-29 18:22:55 +0300

    build verion is  added

--- app/js/main.js
+++ app/js/main.js
@@ -9,5 +9,33 @@ $(function () {
 
 
 
+    $('.strategie__slider-inner').slick({
+        infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        lazyLoad: 'ondemand',
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 940,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
 });
 
